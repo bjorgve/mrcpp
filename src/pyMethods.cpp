96050a3ad1f8cb10531de7a7372845b016dfc4ab
bjorgve <magnbjor@gmail.com>
2018-03-21 10:19:13 +0100

    Move methods to separate file

---
+++ src/pyMethods.cpp
@@ -0,0 +1,50 @@
+/*
+ *  \date Mar 20, 2018
+ *  \author Magnar Bjørgve <magnar.bjorgve@uit.no> \n
+ *          Hylleraas Centre for Quantum Molecular Sciences \n
+ *          UiT - The Arctic University of Norway
+ */
+
+#include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+
+#include "FunctionTree.h"
+#include "FunctionTreeVector.h"
+#include "add.h"
+#include "multiply.h"
+#include "apply.h"
+#include "grid.h"
+#include "project.h"
+#include "DerivativeOperator.h"
+#include "ConvolutionOperator.h"
+
+namespace py = pybind11;
+using namespace mrcpp;
+
+template <int D>
+void init_pymethods(py::module &m) {
+
+
+    m.def("add", py::overload_cast<double, FunctionTree<D> &, double , FunctionTree<D> &, double, FunctionTree<D> &, int>(&add<D>));
+
+    m.def("project", py::overload_cast<double, FunctionTree<D> &, RepresentableFunction<D> &, int>(&project<D>));
+    m.def("project", py::overload_cast<double, FunctionTree<1> &, std::function<double (double)>, int>(&project1D));
+    m.def("project", py::overload_cast<double, FunctionTree<2> &, std::function<double (double, double)>, int>(&project2D));
+    m.def("project", py::overload_cast<double, FunctionTree<3> &, std::function<double (double, double, double)>, int>(&project3D));
+
+    m.def("multiply", py::overload_cast<double, FunctionTree<D> &, double, FunctionTree<D> &, FunctionTree<D> &, int >(&multiply<D>));
+    m.def("multiply", py::overload_cast<double, FunctionTree<D> &, FunctionTreeVector<D> &, int >(&multiply<D>));
+
+    m.def("build_grid", &build_grid<D>);
+    m.def("copy_grid", py::overload_cast<FunctionTree<D> &, FunctionTree<D> &, int >(&copy_grid<D>));
+    m.def("copy_grid", py::overload_cast<FunctionTree<D> &, FunctionTreeVector<D> &, int>(&copy_grid<D>));
+    m.def("clear_grid", &clear_grid<D>);
+
+    m.def("apply", py::overload_cast<double, FunctionTree<D> &, ConvolutionOperator<D> &, FunctionTree<D> &, int>(&apply<D>));
+    m.def("apply", py::overload_cast<FunctionTree<D> &, DerivativeOperator<D> &, FunctionTree<D> &, int>(&apply<D>));
+    m.def("dot", &dot<D>);
+}
+
+template void init_pymethods<1>(py::module &m);
+template void init_pymethods<2>(py::module &m);
+template void init_pymethods<3>(py::module &m);
