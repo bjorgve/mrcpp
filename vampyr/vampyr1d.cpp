c1d5be7121707ccccf946e9f9eac45327af68cbf
Magnar Bjørgve <magnbjor@gmail.com>
2018-07-08 15:01:54 +0200

    Cosmetic fixes

--- vampyr/vampyr1d.cpp
+++ vampyr/vampyr1d.cpp
@@ -11,7 +11,6 @@
 #include "pybind11/eigen.h"
 #include "pybind11/functional.h"
 
-#include "trees/BoundingBox.h"
 #include "trees/MultiResolutionAnalysis.h"
 #include "trees/MWTree.h"
 #include "trees/FunctionTree.h"
@@ -24,6 +23,7 @@
 #include "operators/DerivativeOperator.h"
 #include "operators/ABGVOperator.h"
 #include "operators/ConvolutionOperator.h"
+
 #include "treebuilders/add.h"
 #include "treebuilders/multiply.h"
 #include "treebuilders/apply.h"
@@ -41,39 +41,35 @@ PYBIND11_MODULE(vampyr1d, m) {
 m.def("project", py::overload_cast<double, FunctionTree<1> &, std::function<double (double)>, int>(&project1D),
     py::arg("prec"), py::arg("out"), py::arg("func"), py::arg("maxIter")= -1);
 
-
 py::class_<PyBoundingBox<1>> (m, "BoundingBox1D")
-.def(py::init<int, py::array_t<int>, py::array_t <int>>())
-.def(py::init<int, int *,  int *>()) //1D cases can be initialized without array type input
-.def("getScale", &PyBoundingBox<1>::getScale);
+    .def(py::init<int, py::array_t<int>, py::array_t <int>>())
+    .def(py::init<int, int *,  int *>()) //1D cases can be initialized without array type input
+    .def("getScale", &PyBoundingBox<1>::getScale);
 
 py::class_<MultiResolutionAnalysis<1>> (m, "MultiResolutionAnalysis1D")
-.def(py::init<PyBoundingBox<1>, ScalingBasis, int>())
-.def("getOrder", &MultiResolutionAnalysis<1>::getOrder)
-.def("getMaxDepth", &MultiResolutionAnalysis<1>::getMaxDepth)
-.def("getMaxScale", &MultiResolutionAnalysis<1>::getMaxScale);
+    .def(py::init<PyBoundingBox<1>, ScalingBasis, int>())
+    .def("getOrder", &MultiResolutionAnalysis<1>::getOrder)
+    .def("getMaxDepth", &MultiResolutionAnalysis<1>::getMaxDepth)
+    .def("getMaxScale", &MultiResolutionAnalysis<1>::getMaxScale);
 
 py::class_<MWTree<1>> mwtree(m, "MWTree");
 mwtree
-.def(py::init<MultiResolutionAnalysis<1>>())
-.def("getSquareNorm", &MWTree<1>::getSquareNorm);
+    .def(py::init<MultiResolutionAnalysis<1>>())
+    .def("getSquareNorm", &MWTree<1>::getSquareNorm);
 
 py::class_<FunctionTree<1>> functree(m, "FunctionTree1D", mwtree);
 functree
-.def(py::init<MultiResolutionAnalysis<1>>())
-.def("integrate", &FunctionTree<1>::integrate)
-.def("clear", &FunctionTree<1>::clear)
-.def("normalize", &FunctionTree<1>::normalize)
-.def("evalf", py::overload_cast<double>(&FunctionTree<1>::evalf));
-
-
+    .def(py::init<MultiResolutionAnalysis<1>>())
+    .def("integrate", &FunctionTree<1>::integrate)
+    .def("clear", &FunctionTree<1>::clear)
+    .def("normalize", &FunctionTree<1>::normalize)
+    .def("evalf", py::overload_cast<double>(&FunctionTree<1>::evalf));
 
 m.def("apply", py::overload_cast<double, FunctionTree<1> &, ConvolutionOperator<1> &, FunctionTree<1> &, int>(&apply<1>),
-        py::arg("prec"), py::arg("out"), py::arg("oper"), py::arg("inp"), py::arg("maxIter") = -1);
+    py::arg("prec"), py::arg("out"), py::arg("oper"), py::arg("inp"), py::arg("maxIter") = -1);
 m.def("apply", py::overload_cast<FunctionTree<1> &, DerivativeOperator<1> &, FunctionTree<1> &, int>(&apply<1>));
 
-bases(m);
-
+bases(m); // Import bases
 
 m.def("add", py::overload_cast<double, FunctionTree<1> &, double, FunctionTree<1> &, double, FunctionTree<1> &, int>(&add<1>),
     py::arg("prec"), py::arg("out"), py::arg("a"),  py::arg("tree_a"), py::arg("b"), py::arg("tree_b"), py::arg("maxIter") = -1,
@@ -83,20 +79,13 @@ m.def("project", py::overload_cast<double, FunctionTree<1> &, RepresentableFunct
     py::arg("prec"), py::arg("out"), py::arg("inp"), py::arg("maxIter")= -1);
 
 m.def("multiply", py::overload_cast<double, FunctionTree<1> &, double, FunctionTree<1> &, FunctionTree<1> &, int >(&multiply<1>),
-        py::arg("prec"), py::arg("out"), py::arg("c"), py::arg("tree_a"), py::arg("tree_b"), py::arg("maxIter") = -1,
-        "Multiplies two function trees");
-//m.def("multiply", py::overload_cast<double, FunctionTree<D> &, PyFunctionTreeVector<D> &, int >(&multiply<D>));
+    py::arg("prec"), py::arg("out"), py::arg("c"), py::arg("tree_a"), py::arg("tree_b"), py::arg("maxIter") = -1,
+    "Multiplies two function trees");
 
 py::class_<DerivativeOperator<1>> deriv(m, "Derivative Operator");
 deriv
-.def(py::init<MultiResolutionAnalysis<1>>());
+    .def(py::init<MultiResolutionAnalysis<1>>());
 
 py::class_<ABGVOperator<1>> (m, "ABGVOperator", deriv)
-.def(py::init< MultiResolutionAnalysis<1> &, double, double >());
-//
-//
-//py::class_<ConvolutionOperator<1>> convop(m, "ConvolutionOperator");
-//convop
-//.def(py::init<MultiResolutionAnalysis<1> &, double>());
-
+    .def(py::init< MultiResolutionAnalysis<1> &, double, double >());
 }
--- vampyr/vampyr2d.cpp
+++ vampyr/vampyr2d.cpp
@@ -38,42 +38,38 @@ void bases(py::module &);
 
 PYBIND11_MODULE(vampyr2d, m) {
 
-
 m.def("project", py::overload_cast<double, FunctionTree<2> &, std::function<double (double, double)>, int>(&project2D),
     py::arg("prec"), py::arg("out"), py::arg("func"), py::arg("maxIter")= -1);
 
 py::class_<PyBoundingBox<2>> (m, "BoundingBox2D")
-.def(py::init<int, py::array_t<int>, py::array_t <int>>())
-.def(py::init<int, int *,  int *>()) //1D cases can be initialized without array type input
-.def("getScale", &PyBoundingBox<2>::getScale);
+    .def(py::init<int, py::array_t<int>, py::array_t <int>>())
+    .def(py::init<int, int *,  int *>()) //1D cases can be initialized without array type input
+    .def("getScale", &PyBoundingBox<2>::getScale);
 
 py::class_<MultiResolutionAnalysis<2>> (m, "MultiResolutionAnalysis2D")
-.def(py::init<PyBoundingBox<2>, ScalingBasis, int>())
-.def("getOrder", &MultiResolutionAnalysis<2>::getOrder)
-.def("getMaxDepth", &MultiResolutionAnalysis<2>::getMaxDepth)
-.def("getMaxScale", &MultiResolutionAnalysis<2>::getMaxScale);
+    .def(py::init<PyBoundingBox<2>, ScalingBasis, int>())
+    .def("getOrder", &MultiResolutionAnalysis<2>::getOrder)
+    .def("getMaxDepth", &MultiResolutionAnalysis<2>::getMaxDepth)
+    .def("getMaxScale", &MultiResolutionAnalysis<2>::getMaxScale);
 
 py::class_<MWTree<2>> mwtree(m, "MWTree");
 mwtree
-.def(py::init<MultiResolutionAnalysis<2>>())
-.def("getSquareNorm", &MWTree<2>::getSquareNorm);
+    .def(py::init<MultiResolutionAnalysis<2>>())
+    .def("getSquareNorm", &MWTree<2>::getSquareNorm);
 
 py::class_<FunctionTree<2>> functree(m, "FunctionTree2D", mwtree);
 functree
-.def(py::init<MultiResolutionAnalysis<2>>())
-.def("integrate", &FunctionTree<2>::integrate)
-.def("clear", &FunctionTree<2>::clear)
-.def("normalize", &FunctionTree<2>::normalize)
-.def("evalf", py::overload_cast<double, double>(&FunctionTree<2>::evalf));
-
-
+    .def(py::init<MultiResolutionAnalysis<2>>())
+    .def("integrate", &FunctionTree<2>::integrate)
+    .def("clear", &FunctionTree<2>::clear)
+    .def("normalize", &FunctionTree<2>::normalize)
+    .def("evalf", py::overload_cast<double, double>(&FunctionTree<2>::evalf));
 
 m.def("apply", py::overload_cast<double, FunctionTree<2> &, ConvolutionOperator<2> &, FunctionTree<2> &, int>(&apply<2>),
-        py::arg("prec"), py::arg("out"), py::arg("oper"), py::arg("inp"), py::arg("maxIter") = -1);
+    py::arg("prec"), py::arg("out"), py::arg("oper"), py::arg("inp"), py::arg("maxIter") = -1);
 m.def("apply", py::overload_cast<FunctionTree<2> &, DerivativeOperator<2> &, FunctionTree<2> &, int>(&apply<2>));
 
 bases(m);
-
 
 m.def("add", py::overload_cast<double, FunctionTree<2> &, double, FunctionTree<2> &, double, FunctionTree<2> &, int>(&add<2>),
     py::arg("prec"), py::arg("out"), py::arg("a"),  py::arg("tree_a"), py::arg("b"), py::arg("tree_b"), py::arg("maxIter") = -1,
@@ -83,20 +79,13 @@ m.def("project", py::overload_cast<double, FunctionTree<2> &, RepresentableFunct
     py::arg("prec"), py::arg("out"), py::arg("inp"), py::arg("maxIter")= -1);
 
 m.def("multiply", py::overload_cast<double, FunctionTree<2> &, double, FunctionTree<2> &, FunctionTree<2> &, int >(&multiply<2>),
-        py::arg("prec"), py::arg("out"), py::arg("c"), py::arg("tree_a"), py::arg("tree_b"), py::arg("maxIter") = -1,
-        "Multiplies two function trees");
-//m.def("multiply", py::overload_cast<double, FunctionTree<D> &, PyFunctionTreeVector<D> &, int >(&multiply<D>));
+    py::arg("prec"), py::arg("out"), py::arg("c"), py::arg("tree_a"), py::arg("tree_b"), py::arg("maxIter") = -1,
+    "Multiplies two function trees");
 
 py::class_<DerivativeOperator<2>> deriv(m, "Derivative Operator");
 deriv
-.def(py::init<MultiResolutionAnalysis<2>>());
+    .def(py::init<MultiResolutionAnalysis<2>>());
 
 py::class_<ABGVOperator<2>> (m, "ABGVOperator", deriv)
-.def(py::init< MultiResolutionAnalysis<2> &, double, double >());
-//
-//
-//py::class_<ConvolutionOperator<1>> convop(m, "ConvolutionOperator");
-//convop
-//.def(py::init<MultiResolutionAnalysis<1> &, double>());
-
+    .def(py::init< MultiResolutionAnalysis<2> &, double, double >());
 }
